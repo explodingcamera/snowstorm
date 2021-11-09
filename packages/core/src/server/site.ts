b2223e77d857382839b51f4e8c691aafc111f9db
Henry <mail@henrygressmann.de>
2021-11-09 21:01:40 +0100

    transform mixed esmodules
    Signed-off-by: Henry <mail@henrygressmann.de>

--- packages/core/src/server/site.ts
+++ packages/core/src/server/site.ts
@@ -78,6 +78,9 @@ const viteBaseConfig = (
 		},
 	},
 	build: {
+		commonjsOptions: {
+			transformMixedEsModules: true,
+		},
 		rollupOptions: {
 			input: join(config.internal.snowstormClientFolder, './index.js'),
 		},
