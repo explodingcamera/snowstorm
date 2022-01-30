03e4238c4fde4398dcb6b1096a31908979328247
Henry Gressmann <mail@henrygressmann.de>
2022-01-30 20:58:04 +0100

    chore: start refactoring server-side routing
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

---
+++ packages/router/test/basic.ts
@@ -0,0 +1,54 @@
+import tap from 'tap';
+import { processRoute, Route } from './../src/index.js';
+
+tap.test('', t => {
+	const shouldErr = [''];
+	for (const route of shouldErr) {
+		t.throws(() => processRoute(route));
+	}
+
+	const shouldNotErr: Array<[string, Route]> = [
+		[
+			'test',
+			{
+				path: '/test',
+				name: 'test',
+				parts: ['test'],
+				decorator: undefined,
+			},
+		],
+		[
+			'a/b/c',
+			{
+				path: '/a/b/c',
+				name: 'a/b/c',
+				parts: ['a', 'b', 'c'],
+				decorator: undefined,
+			},
+		],
+		[
+			'a/b.bar',
+			{
+				path: '/a/b',
+				name: 'a/b',
+				parts: ['a', 'b'],
+				decorator: 'bar',
+			},
+		],
+		[
+			'[a]/[b].bar',
+			{
+				path: '/:a/:b',
+				name: '[a]/[b]',
+				parts: [':a', ':b'],
+				decorator: 'bar',
+			},
+		],
+	];
+
+	for (const [route, res] of shouldNotErr) {
+		t.same(processRoute(route), res);
+	}
+
+	t.end();
+});
