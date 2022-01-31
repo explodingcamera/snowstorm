473ade68e0c1ff726f82087222f1036badb77bce
explodingcamera <explodingcamera@users.noreply.github.com>
2022-01-31 19:58:04 +0000

    deploy: 14a3a560528c0033090359d9b38827db65f34232

--- assets/getting-started.43e33a29.js
+++ assets/getting-started.43e33a29.js
@@ -1,4 +1,4 @@
-import{j as e}from"./jsx-runtime.ccab6159.js";import"./vendor.aa87bd79.js";import{U as r}from"./index.cd6cf992.js";const l="Getting Started";function p(n={}){const{wrapper:t}=n.components||{};return t?e.exports.jsx(t,Object.assign({},n,{children:e.exports.jsx(o,{})})):o();function o(){const s=Object.assign({blockquote:"blockquote",p:"p",strong:"strong",h2:"h2",ul:"ul",li:"li",code:"code",pre:"pre",span:"span"},n.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsxs(s.blockquote,{children:[`
+import{j as e}from"./jsx-runtime.ccab6159.js";import"./vendor.aa87bd79.js";import{U as r}from"./index.26ac6963.js";const l="Getting Started";function p(n={}){const{wrapper:t}=n.components||{};return t?e.exports.jsx(t,Object.assign({},n,{children:e.exports.jsx(o,{})})):o();function o(){const s=Object.assign({blockquote:"blockquote",p:"p",strong:"strong",h2:"h2",ul:"ul",li:"li",code:"code",pre:"pre",span:"span"},n.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsxs(s.blockquote,{children:[`
 `,e.exports.jsxs(s.p,{children:[e.exports.jsx(s.strong,{children:"Welcome to Snowstorm's Documentation!"}),e.exports.jsx("br",{}),`
 Please note that this site is still incomplete.
 I'm actively working on version 1.0, which will hopefully be ready in the coming months. Until then, expect bugs and breaking changes.`]}),`
@@ -27,6 +27,6 @@ $ `,e.exports.jsx(s.span,{className:"token builtin class-name",children:"echo"})
 `,e.exports.jsx(s.span,{className:"token comment",children:"# start the development server"}),`
 $ npx snowstorm dev
 `]})}),`
-`,e.exports.jsx("pre",{className:"language-terminal",children:e.exports.jsxs(s.p,{children:[e.exports.jsx("b",{children:"INFO"}),` starting snowstorm v0.0.1 (development mode)
+`,e.exports.jsx("pre",{className:"language-terminal",children:e.exports.jsxs(s.p,{children:[e.exports.jsx("b",{children:"INFO"}),` starting snowstorm v0.10.0 (development mode)
 `,e.exports.jsx("b",{children:"INFO"}),` started in 19ms
 `,e.exports.jsx("b",{children:"INFO"})," listening on ",e.exports.jsx("u",{children:"http:/\u200B/localhost:2020/"})]})})]})}}export{p as default,l as title};
