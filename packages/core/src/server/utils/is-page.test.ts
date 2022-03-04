1f4c51b1472fdd800205cce8579a8db99c9ec7ef
Henry Gressmann <mail@henrygressmann.de>
2022-03-04 16:38:07 +0100

    chore: update to new react rc
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- packages/core/src/server/utils/is-page.test.ts
+++ packages/core/src/server/utils/is-page.test.ts
@@ -5,8 +5,6 @@ void tap.test('fileIsPage', async t => {
 	const pass = ['index.js', 'pages/page.tsx', 'page.md', 'page.mdx'];
 	const fail = ['', 'index.html', 'index.js.old'];
 
-	console.log(fileIsPage('index.js'));
-
 	for (const file of pass) t.ok(fileIsPage(file));
 	for (const file of fail) t.notOk(fileIsPage(file));
 });
